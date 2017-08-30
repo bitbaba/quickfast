10c14403824e88969a4324d3b991b929f6cb5780
bitbaba <imzhhwu@gmail.com>
2017-08-30 17:23:00 +0800

    update download links for xerces and zmq

--- depends/xercesc-3.1.4.mk
+++ depends/xercesc-3.1.4.mk
@@ -1,7 +1,8 @@
 package=xerces-c
 package_version=3.1.4
 package_out_dir=$${INSTALLROOT}
-package_download_url="http://mirrors.tuna.tsinghua.edu.cn/apache/xerces/c/3/sources/xerces-c-3.1.4.tar.gz"
+#package_download_url="http://mirrors.tuna.tsinghua.edu.cn/apache/xerces/c/3/sources/xerces-c-3.1.4.tar.gz"
+package_download_url="http://archive.apache.org/dist/xerces/c/3/sources/xerces-c-3.1.4.tar.gz"
 package_build_dir=/tmp/$(package)-$(package_version)
 package_tarball=/tmp/$(package)-$(package_version).tar.gz
 
